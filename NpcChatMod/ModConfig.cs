55e02121b3e69a9fab8feff6ed3f41335817c1cb
Mike Landis <landis.michael@gmail.com>
2023-03-04 17:44:59 -0500

    initial prototyping working with open ai, uses config

---
+++ NpcChatMod/ModConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace NpcChatMod {
+    class ModConfig {
+        public string OpenAiUrl { get; set; } = "https://api.openai.com";
+        public string OpenAiBearerToken { get; set; } = "{yourtoken}";
+        public string OpenAiModel { get; set; } = "text-davinci-edit-001";
+        public string OpenAiInstruction { get; set; } = "Act as {characterName} from Stardew Valley and embellish the dialog with details. Limit the size of the response to a few sentences.";
+        public double OpenAiTemperature { get; set; } = 0.5;
+        public int OpenAiTimeoutMillis { get; set; } = 5000;
+        public string OpenAiEdits { get; set; } = "/v1/edits";
+        public long OpenAiMaxBufferSize { get; set; } = 1024;
+    }
+}
--- NpcChatMod/ModEntry.cs
+++ NpcChatMod/ModEntry.cs
@@ -1,40 +1,117 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿using Newtonsoft.Json;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
-using StardewModdingAPI.Utilities;
-using StardewValley;
+using StardewValley.Menus;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
 
-namespace NpcChatMod
-{
+namespace NpcChatMod {
+
+    public class AiEditRequest {
+        public string model { get; set; }
+        public string input { get; set; }
+        public string instruction { get; set; }
+        public double temperature { get; set; }
+        public AiEditRequest(string model, string input, string instruction, double temperature) {
+            this.model = model;
+            this.input = input;
+            this.instruction = instruction;
+            this.temperature = temperature;
+        }
+    }
+    public class AiEditResponseChoice {
+        public string text { get; set; }
+
+    }
+    public class AiEditResponse {
+
+        public AiEditResponseChoice[] choices { get; set; }
+    }
+
     /// <summary>The mod entry point.</summary>
-    public class ModEntry : Mod
-    {
+    public class ModEntry : Mod {
+
+        private HttpClient Client;
+        private ModConfig Config;
+
         /*********
         ** Public methods
         *********/
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
-        public override void Entry(IModHelper helper)
-        {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        public override void Entry(IModHelper helper) {
+            this.Config = this.Helper.ReadConfig<ModConfig>();
+            this.Client = new HttpClient();
+            Client.BaseAddress = new Uri(this.Config.OpenAiUrl);
+            Client.MaxResponseContentBufferSize = this.Config.OpenAiMaxBufferSize;
+            Client.Timeout = TimeSpan.FromMilliseconds(this.Config.OpenAiTimeoutMillis);
+
+            // Add an Accept header for JSON format.
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.Config.OpenAiBearerToken);
+
+            helper.Events.Display.MenuChanged += this.OnMenuChanged;
         }
 
 
-        /*********
+        /*****************
         ** Private methods
-        *********/
-        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
-        {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
+        ******************/
 
-            // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+        private void OnMenuChanged(object sender, MenuChangedEventArgs e) {
+            if (e.NewMenu is DialogueBox dialogue) {
+
+                var newStack = new Stack<string>(dialogue.characterDialoguesBrokenUp.Count);
+
+                foreach (var dialogueStackItem in dialogue.characterDialoguesBrokenUp) {
+                    var editedItem = this.getAIEdit(dialogue.characterDialogue.speaker.name, dialogueStackItem);
+                    this.Monitor.Log($"Edited Item: {dialogueStackItem} --> {editedItem}", LogLevel.Debug);
+
+                    var chunks = this.breakText(editedItem);
+                    for (int i = chunks.Length - 1; i >= 0; i--) {
+                        var chunk = chunks[i];
+                        newStack.Push(chunk);
+                    }
+                }
+
+                dialogue.characterDialoguesBrokenUp.Clear();
+                dialogue.characterDialoguesBrokenUp = newStack;
+            }
+        }
+
+        private string getAIEdit(string characterName, string input) {
+            var model = this.Config.OpenAiModel;
+            var instruction = this.Config.OpenAiInstruction.Replace("{characterName}", characterName);
+//                $"Act as {characterName} from Stardew Valley and embellish the dialog with details. Limit the size of the response to a few sentences.";
+
+            var payload = new AiEditRequest(model, input, instruction, this.Config.OpenAiTemperature);
+            var payloadString = JsonConvert.SerializeObject(payload);
+            var payloadContent = new StringContent(payloadString, System.Text.Encoding.UTF8, "application/json");
+
+            try {
+                HttpResponseMessage response = this.Client.PostAsync(this.Config.OpenAiEdits, payloadContent).GetAwaiter().GetResult();  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+
+                if (response.IsSuccessStatusCode) {
+                    // Parse the response body.
+                    var content = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();  //Make sure to add a reference to System.Net.Http.Formatting.dll
+                    var body = JsonConvert.DeserializeObject<AiEditResponse>(content);
+                    if (body.choices.Length >= 1) {
+                        var aiEdit = Regex.Replace(body.choices[0].text, @"\t|\r", "");
+                        return aiEdit;
+                    }
+                }
+                this.Monitor.Log($"API Error: {response.StatusCode} ({response.ReasonPhrase} {payloadContent})", LogLevel.Error);
+            } catch (Exception e) {
+                this.Monitor.Log($"API Exception: {e.Message} ({e})", LogLevel.Error);
+            }
+            return input;
+        }
+
+        string[] breakText(string input) {
+            return input.Split(new[] { "\n\n" }, 25, StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
