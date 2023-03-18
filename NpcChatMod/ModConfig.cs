0368e11fa6750183ddd51d130b4be6d302ea4c18
Mike Landis <landis.michael@gmail.com>
2023-03-17 21:57:47 -0400

    prepare release

--- NpcChatMod/ModConfig.cs
+++ NpcChatMod/ModConfig.cs
@@ -6,7 +6,7 @@ namespace NpcChatMod {
         public string OpenAiUrl { get; set; } = "https://api.openai.com";
         public string OpenAiBearerToken { get; set; } = "{yourtoken}";
         public string OpenAiModel { get; set; } = "text-davinci-edit-001";
-        public string OpenAiInstruction { get; set; } = "Speak as {characterName} (from Stardew Valley). Embellish the dialog with details. Limit the size of the response to 3-7 sentences. Location: {insideOrOutside} in {location}. When: {timeOfDay} in {season}. People nearby include: {characters}.";
+        public string OpenAiInstruction { get; set; } = "Act as {characterName} from Stardew Valley talking to a farmer. We are {insideOrOutside} in {location}. When: {timeOfDay} in {season}. People other than us nearby are: {characters}. Limit the text to 3-7 sentences.";
         public double OpenAiTemperature { get; set; } = 0.5;
         public int OpenAiTimeoutMillis { get; set; } = 5000;
         public string OpenAiEdits { get; set; } = "/v1/edits";
